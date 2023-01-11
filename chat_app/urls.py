36d1c91bf16d1da36198b8bc3502822ddcf82482
bigtail <erichardin0514@gmail.com>
2023-01-11 12:00:00 +0100

    fist commit

--- chat_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('chat/', views.Home.as_view(),name="chat"),
-    path('',views.Signup.as_view(),name="signup"),
-    path('login/',views.LoginView.as_view(),name="login"),
-    path('logout/',views.logoutView,name="logout")
-]
